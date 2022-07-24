487f20ddea3a2793489c6dd4c0af6fcd4f6503e8
Anton Panteleev <panteleev23@gmail.com>
2022-07-24 19:42:49 +1000

    Test - include btn details

--- cypress/e2e/01 - JavaScript Drum Kit/drum_spec.cy.js
+++ cypress/e2e/01 - JavaScript Drum Kit/drum_spec.cy.js
@@ -1,14 +1,32 @@
 describe('empty spec', () => {
   it('passes', () => {
     cy.visit('/javascript30/01 - JavaScript Drum Kit/index.html ')
-    cy.contains('clap')
-    cy.contains('hihat')
-    cy.contains('kick')
-    cy.contains('openhat')
-    cy.contains('boom')
-    cy.contains('ride')
-    cy.contains('snare')
-    cy.contains('tom')
-    cy.contains('tink')
+
+    cy.get(`.keys > [data-key="65"]`).contains('A')
+    cy.get(`.keys > [data-key="65"]`).contains('clap')
+
+    cy.get(`.keys > [data-key="83"]`).contains('S')
+    cy.get(`.keys > [data-key="83"]`).contains('hihat')
+
+    cy.get(`.keys > [data-key="68"]`).contains('D')
+    cy.get(`.keys > [data-key="68"]`).contains('kick')
+
+    cy.get(`.keys > [data-key="70"]`).contains('F')
+    cy.get(`.keys > [data-key="70"]`).contains('openhat')
+
+    cy.get(`.keys > [data-key="71"]`).contains('G')
+    cy.get(`.keys > [data-key="71"]`).contains('boom')
+
+    cy.get(`.keys > [data-key="72"]`).contains('H')
+    cy.get(`.keys > [data-key="72"]`).contains('ride')
+
+    cy.get(`.keys > [data-key="74"]`).contains('J')
+    cy.get(`.keys > [data-key="74"]`).contains('snare')
+
+    cy.get(`.keys > [data-key="75"]`).contains('K')
+    cy.get(`.keys > [data-key="75"]`).contains('tom')
+
+    cy.get(`.keys > [data-key="76"]`).contains('L')
+    cy.get(`.keys > [data-key="76"]`).contains('tink')
     })
 })
