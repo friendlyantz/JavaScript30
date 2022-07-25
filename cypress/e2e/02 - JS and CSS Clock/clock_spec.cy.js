86a28fabfdac0333bc58dab17420baa5468c7ecb
Anton Panteleev <panteleev23@gmail.com>
2022-07-26 07:51:54 +1000

    TDD clock hands origin offset. refac tests and linting

--- cypress/e2e/02 - JS and CSS Clock/clock_spec.cy.js
+++ cypress/e2e/02 - JS and CSS Clock/clock_spec.cy.js
@@ -1,9 +1,24 @@
 describe('clock', () => {
-  it('passes', () => {
+  before(() => {
     cy.visit('/javascript30/02 - JS and CSS Clock/index.html ');
+  });
+
+  it('start with hour, minute and second hands starting at 10h:10m:20s', () => {
 
     cy.get('.clock > .clock-face > .hour-hand');
     cy.get('.clock > .clock-face > .min-hand');
     cy.get('.clock > .clock-face > .second-hand');
+
+    cy.get('.clock > .clock-face > .hand')
+      .should(
+        'have.css',
+        'transform-origin',
+        '150px 3px'//,'right' unable to assert non-absolute value such as 'right'
+      );
+
   });
+
+  xit('second hand ticks every second'), () => {
+    //TODO
+  };
 });
